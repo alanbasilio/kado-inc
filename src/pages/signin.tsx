7c59dca9bec004e49b4c7199d90257c5fe98137e
Luis Henrique Trindade <luishenriquetp@gmail.com>
2022-08-11 17:32:27 +0200

    add packages and configs

--- src/pages/signin.tsx
+++ src/pages/signin.tsx
@@ -1,20 +1,43 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import { Col, Row } from "react-bootstrap";
+import { Button, Col, Form, InputGroup, Row } from "react-bootstrap";
 
 import Layout from "../components/layout";
 import styles from "../styles/Home.module.scss";
 
-const Home: NextPage = () => {
+const Signin: NextPage = () => {
   return (
     <div className={styles.container}>
       <Layout>
         <Row>
-          <Col></Col>
+          <Col className="md-6">
+            <h1>Sign in</h1>
+            <p>Welcome back, you’ve been missed!</p>
+            <Button variant="light">Sign in with Google</Button>{" "}
+            <InputGroup className="d-flex justify-content-center">
+              <InputGroup.Text id="basic-addon1">@</InputGroup.Text>
+              <Form.Control
+                placeholder="Your Email"
+                aria-label="Email"
+                aria-describedby="basic-addon1"
+              />
+            </InputGroup>
+            <InputGroup className="d-flex justify-content-center">
+              <InputGroup.Text id="basic-addon1">🔒</InputGroup.Text>
+              <Form.Control
+                placeholder="Password"
+                aria-label="Password"
+                aria-describedby="basic-addon1"
+              />
+            </InputGroup>
+            <h2>Forgot Password?</h2>
+            <Button type="submit">Sign in</Button>{" "}
+            <p>You can also sign in with a Magic link</p>
+          </Col>
         </Row>
       </Layout>
     </div>
   );
 };
 
-export default Home;
+export default Signin;
