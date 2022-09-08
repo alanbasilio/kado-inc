c137c345049d5e7c48de806863ed7860503d1fbc
Luis Henrique Trindade <luishenriquetp@gmail.com>
2022-09-08 13:02:19 -0300

    add my projects card

--- src/components/card-project/index.tsx
+++ src/components/card-project/index.tsx
@@ -5,44 +5,44 @@ import { Card, Col, Row } from "react-bootstrap";
 
 const CardProject: NextPage = () => {
   return (
-    <Card className="p-2">
+    <Card className="p-2 mb-2">
       <Row>
         <Col className="p-2">
-          <h4 className="mt-2">Boxboard Multipurpose Ui Kit</h4>
+          <h5 className="mt-2">Boxboard Multipurpose Ui Kit</h5>
           <h6 className="mb-2 text-muted">Boxboard</h6>
           <p className="mb-2 text-muted">15 Days left</p>
 
           <Image
             className="img-fluid border rounded-circle"
-            src="/images/design.png"
+            src="/images/jerome.png"
             width="40"
             height="40"
             alt="React Bootstrap logo"
           />
           <Image
             className="img-fluid border rounded-circle"
-            src="/images/design.png"
+            src="/images/jerome.png"
             width="40"
             height="40"
             alt="React Bootstrap logo"
           />
           <Image
             className="img-fluid border rounded-circle"
-            src="/images/design.png"
+            src="/images/jerome.png"
             width="40"
             height="40"
             alt="React Bootstrap logo"
           />
           <Image
             className="img-fluid border rounded-circle"
-            src="/images/design.png"
+            src="/images/jerome.png"
             width="40"
             height="40"
             alt="React Bootstrap logo"
           />
           <Image
             className="img-fluid border rounded-circle"
-            src="/images/design.png"
+            src="/images/jerome.png"
             width="40"
             height="40"
             alt="React Bootstrap logo"
--- src/pages/my-projects.tsx
+++ src/pages/my-projects.tsx
@@ -1,13 +1,43 @@
 import type { NextPage } from "next";
+import { Button, Col, Form, Row, Card } from "react-bootstrap";
+import { MdOutlineAlternateEmail } from "react-icons/md";
+import { useState } from "react";
+import Link from "next/link";
+import Image from "next/image";
 
 import Layout from "../components/dashboard-layout";
+import ProgBar from "../components/progress-bar/progress-bar";
+import CardProject from "../components/card-project";
+import CardProjectProgbar from "../components/card-project-progbar";
 
-const Home: NextPage = () => {
+const MyProjects: NextPage = () => {
+  const [active, setActive] = useState("");
   return (
     <Layout>
-      <h1>My Projects</h1>
+      <Row className="mt-2">
+        <Col md={12} className="mb-2">
+          <h3 className="mb-4">My Projects</h3>
+        </Col>
+        <Col md={4} className="mb-2">
+          <h5 className="mb-2 fw-semibold">TODO</h5>
+          <CardProject />
+          <CardProject />
+        </Col>
+
+        <Col md={4} className="mb-2">
+          <h5 className="mb-2 fw-semibold">ONGOING</h5>
+          <CardProjectProgbar />
+          <CardProjectProgbar />
+        </Col>
+
+        <Col md={4} className="mb-2">
+          <h5 className="mb-2 fw-semibold">COMPLETED</h5>
+          <CardProjectProgbar />
+          <CardProjectProgbar />
+        </Col>
+      </Row>
     </Layout>
   );
 };
 
-export default Home;
+export default MyProjects;
