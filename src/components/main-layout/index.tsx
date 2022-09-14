2fb6d2d932508c834655de79f0944de39fc32281
Luís Henrique <luishenriquetp@gmail.com>
2022-09-14 12:09:49 -0300

    home page
    Including home page

--- src/components/main-layout/index.tsx
+++ src/components/main-layout/index.tsx
@@ -10,114 +10,115 @@ import Head from "next/head";
 import { useEffect, useState } from "react";
 
 const Layout = (props) => {
-  const [user, setUser] = useState(null);
+	const [user, setUser] = useState(null);
 
-  useEffect(() => {
-    // storing input name
-    if (localStorage.getItem("user_kado")) {
-      setUser(JSON.parse(localStorage.getItem("user_kado")));
-    } else {
-      setUser(null);
-    }
-  }, []);
+	useEffect(() => {
+		// storing input name
+		if (localStorage.getItem("user_kado")) {
+			setUser(JSON.parse(localStorage.getItem("user_kado")));
+		} else {
+			setUser(null);
+		}
+	}, []);
 
-  const logout = () => {
-    setUser(null);
-    localStorage.removeItem("user_kado");
-    localStorage.removeItem("token_kado");
-  };
+	const logout = () => {
+		setUser(null);
+		localStorage.removeItem("user_kado");
+		localStorage.removeItem("token_kado");
+	};
 
-  return (
-    <div className={`${styles.layout} d-flex flex-column pt-8`}>
-      <Head>
-        <title>Kado Inc</title>
-        <meta name="description" content="Kado Inc" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <Navbar bg="light" expand="lg" fixed="top">
-        <Container>
-          <Link href="/" passHref>
-            <Navbar.Brand>
-              <Image
-                src="/images/logo.svg"
-                width="96"
-                height="26"
-                alt="React Bootstrap logo"
-              />
-            </Navbar.Brand>
-          </Link>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse
-            id="basic-navbar-nav"
-            className="justify-content-end"
-          >
-            {user ? (
-              <>
-                <Navbar.Text className="me-1">
-                  {`Welcome, ${user.first_name}`}
-                </Navbar.Text>
-                <Button variant="outline-primary" onClick={() => logout()}>
-                  Logout
-                </Button>
-              </>
-            ) : (
-              <>
-                <Navbar.Text className="me-1">
-                  {props.signin ? "New to Kado?" : "Have an account?"}
-                </Navbar.Text>
-                <Link href={props.signin ? "/signup" : "/signin"} passHref>
-                  <Button variant="outline-primary">
-                    {props.signin ? "Create an account" : "Log in"}
-                  </Button>
-                </Link>
-              </>
-            )}
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-      <Container
-        as={"main"}
-        className="flex-fill d-flex flex-column justify-content-center py-3"
-      >
-        {props.children}
-      </Container>
-      <Container
-        fluid
-        as={"footer"}
-        className="d-flex flex-column justify-content-end align-items-center py-2"
-      >
-        <Container>
-          <Row>
-            <Col className="text-center d-md-flex justify-content-between align-items-center">
-              <p className="m-0">© 2022 Kado. All rights reserved.</p>
-              <Nav className="d-inline-flex">
-                <Nav.Item>
-                  <Link href="/" passHref>
-                    <Nav.Link>Terms</Nav.Link>
-                  </Link>
-                </Nav.Item>
-                <Nav.Item>
-                  <Link href="/" passHref>
-                    <Nav.Link>Privacy</Nav.Link>
-                  </Link>
-                </Nav.Item>
-                <Nav.Item>
-                  <Link href="/" passHref>
-                    <Nav.Link>Security</Nav.Link>
-                  </Link>
-                </Nav.Item>
-                <Nav.Item>
-                  <Link href="/" passHref>
-                    <Nav.Link>Get in touch</Nav.Link>
-                  </Link>
-                </Nav.Item>
-              </Nav>
-            </Col>
-          </Row>
-        </Container>
-      </Container>
-    </div>
-  );
+	return (
+		<div className={`${styles.layout} d-flex flex-column pt-8`}>
+			<Head>
+				<title>Kado Inc</title>
+				<meta name="description" content="Kado Inc" />
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
+			<Navbar bg="light" expand="lg" fixed="top">
+				<Container fluid>
+					<Link href="/" passHref>
+						<Navbar.Brand>
+							<Image
+								src="/images/logo.svg"
+								width="96"
+								height="26"
+								alt="React Bootstrap logo"
+							/>
+						</Navbar.Brand>
+					</Link>
+					<Navbar.Toggle aria-controls="basic-navbar-nav" />
+					<Navbar.Collapse
+						id="basic-navbar-nav"
+						className="justify-content-end"
+					>
+						{user ? (
+							<>
+								<Navbar.Text className="me-1">
+									{`Welcome, ${user.first_name}`}
+								</Navbar.Text>
+								<Button variant="outline-primary" onClick={() => logout()}>
+									Logout
+								</Button>
+							</>
+						) : (
+							<>
+								<Navbar.Text className="me-1">
+									{props.signin ? "New to Kado?" : "Have an account?"}
+								</Navbar.Text>
+								<Link href={props.signin ? "/signup" : "/signin"} passHref>
+									<Button variant="outline-primary">
+										{props.signin ? "Create an account" : "Log in"}
+									</Button>
+								</Link>
+							</>
+						)}
+					</Navbar.Collapse>
+				</Container>
+			</Navbar>
+			<Container
+				as={"main"}
+				className="flex-fill d-flex flex-column justify-content-center py-3"
+				fluid
+			>
+				{props.children}
+			</Container>
+			<Container
+				fluid
+				as={"footer"}
+				className="d-flex flex-column justify-content-end align-items-center py-2"
+			>
+				<Container>
+					<Row>
+						<Col className="text-center d-md-flex justify-content-between align-items-center">
+							<p className="m-0">© 2022 Kado. All rights reserved.</p>
+							<Nav className="d-inline-flex">
+								<Nav.Item>
+									<Link href="/" passHref>
+										<Nav.Link>Terms</Nav.Link>
+									</Link>
+								</Nav.Item>
+								<Nav.Item>
+									<Link href="/" passHref>
+										<Nav.Link>Privacy</Nav.Link>
+									</Link>
+								</Nav.Item>
+								<Nav.Item>
+									<Link href="/" passHref>
+										<Nav.Link>Security</Nav.Link>
+									</Link>
+								</Nav.Item>
+								<Nav.Item>
+									<Link href="/" passHref>
+										<Nav.Link>Get in touch</Nav.Link>
+									</Link>
+								</Nav.Item>
+							</Nav>
+						</Col>
+					</Row>
+				</Container>
+			</Container>
+		</div>
+	);
 };
 
 export default Layout;
--- src/pages/example.tsx
+++ src/pages/example.tsx
@@ -10,85 +10,117 @@ import Layout from "../components/dashboard-layout";
 import API from "../services";
 
 const Example: NextPage = () => {
-  const [loading, setLoading] = useState(false);
-  const router = useRouter();
+	const [loading, setLoading] = useState(false);
+	const router = useRouter();
 
-  const {
-    register,
-    handleSubmit,
-    reset,
-    formState: { errors },
-  } = useForm();
+	const {
+		register,
+		handleSubmit,
+		reset,
+		formState: { errors },
+	} = useForm();
 
-  const onSubmit = (data) => {
-    setLoading(true);
-    API.post("/user", data)
-      .then((response) => {
-        setLoading(false);
-        reset();
-        swal("Success", "Message", "success").then(function () {
-          router.push("/");
-        });
-      })
-      .catch((err) => {
-        setLoading(false);
-        swal(
-          "Error",
-          err.response?.data?.message
-            ? err.response?.data?.message
-            : "An error occured: " + err,
-          "error"
-        );
-      });
-  };
+	const onSubmit = (data) => {
+		setLoading(true);
+		API.post("/user", data)
+			.then((response) => {
+				setLoading(false);
+				reset();
+				swal("Success", "Message", "success").then(function () {
+					router.push("/");
+				});
+			})
+			.catch((err) => {
+				setLoading(false);
+				swal(
+					"Error",
+					err.response?.data?.message
+						? err.response?.data?.message
+						: "An error occured: " + err,
+					"error"
+				);
+			});
+	};
 
-  return (
-    <Layout>
-      <Row
-        className="justify-content-center"
-        as="form"
-        onSubmit={handleSubmit(onSubmit)}
-      >
-        <Col md={6}>
-          <Form.Group className="mb-2">
-            <Form.Control
-              placeholder="Test"
-              type="text"
-              isInvalid={errors.test ? true : false}
-              {...register("test", { required: true })}
-            />
-          </Form.Group>
-        </Col>
+	return (
+		<Layout>
+			<Row className="my-3">
+				<h1>Form</h1>
+			</Row>
+			<Row
+				className="justify-content-center"
+				as="form"
+				onSubmit={handleSubmit(onSubmit)}
+			>
+				<Col md={6}>
+					<Form.Group className="mb-2">
+						<Form.Control
+							placeholder="Test"
+							type="text"
+							isInvalid={errors.test ? true : false}
+							{...register("test", { required: true })}
+						/>
+					</Form.Group>
+				</Col>
 
-        <Col md={6}>
-          <Form.Group className="mb-2">
-            <Form.Control
-              placeholder="Test 2"
-              type="text"
-              isInvalid={errors.test2 ? true : false}
-              {...register("test2", { required: true })}
-            />
-          </Form.Group>
-        </Col>
+				<Col md={6}>
+					<Form.Group className="mb-2">
+						<Form.Control
+							placeholder="Test 2"
+							type="text"
+							isInvalid={errors.test2 ? true : false}
+							{...register("test2", { required: true })}
+						/>
+					</Form.Group>
+				</Col>
 
-        <InputGroup className="mb-2">
-          <InputGroup.Text>
-            <MdOutlineAlternateEmail />
-          </InputGroup.Text>
-          <Form.Control
-            placeholder="Your E-mail"
-            type="email"
-            isInvalid={errors.email ? true : false}
-            {...register("email", { required: true })}
-          />
-        </InputGroup>
+				<InputGroup className="mb-2">
+					<InputGroup.Text>
+						<MdOutlineAlternateEmail />
+					</InputGroup.Text>
+					<Form.Control
+						placeholder="Your E-mail"
+						type="email"
+						isInvalid={errors.email ? true : false}
+						{...register("email", { required: true })}
+					/>
+				</InputGroup>
 
-        <Button variant="primary" type="submit" disabled={!loading}>
-          Continue
-        </Button>
-      </Row>
-    </Layout>
-  );
+				<Button variant="primary" type="submit" disabled={!loading}>
+					Continue
+				</Button>
+			</Row>
+			<Row className="my-3">
+				<h1>Buttons (Two in a row)</h1>
+			</Row>
+			<Row className="my-3">
+				<Col md={2}>
+					<div>
+						<Button
+							variant="primary"
+							type="submit"
+							size="sm"
+							disabled={loading}
+						>
+							Webdesign
+						</Button>
+					</div>
+				</Col>
+				<Col md={2}>
+					<div>
+						<Button
+							variant="primary"
+							type="submit"
+							size="sm"
+							disabled={loading}
+						>
+							Unisense
+						</Button>
+					</div>
+				</Col>
+			</Row>
+		</Layout>
+	);
 };
 
 export default Example;
